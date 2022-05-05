0155f227dbbdfb1e2295469e5a4d35e02849969e
tpkeeper <tpkeeper.me@gmail.com>
2022-05-05 11:53:29 +0800

    impl rbank genesis

--- x/rbank/genesis.go
+++ x/rbank/genesis.go
@@ -11,12 +11,22 @@ import (
 func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) {
 	// this line is used by starport scaffolding # genesis/module/init
 	k.SetParams(ctx, genState.Params)
+	for _, prefix := range genState.AddressPrefix {
+		if len(prefix.Denom) == 0 || len(prefix.AccAddressPrefix) == 0 || len(prefix.ValAddressPrefix) == 0 {
+			panic("not valid address prefix")
+		}
+		k.SetAccAddressPrefix(ctx, prefix.Denom, prefix.AccAddressPrefix)
+		k.SetValAddressPrefix(ctx, prefix.Denom, prefix.ValAddressPrefix)
+	}
 }
 
 // ExportGenesis returns the capability module's exported genesis.
 func ExportGenesis(ctx sdk.Context, k keeper.Keeper) *types.GenesisState {
 	genesis := types.DefaultGenesis()
 	genesis.Params = k.GetParams(ctx)
+
+	list := k.GetAddressPrefixList(ctx)
+	genesis.AddressPrefix = list
 
 	// this line is used by starport scaffolding # genesis/module/export
 
