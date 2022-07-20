e3e8c21775a53db2c4e0cf97589ad1339a1385bf
tpkeeper <tpkeeper.me@gmail.com>
2022-07-20 11:43:13 +0800

    should check pool address

--- x/ledger/keeper/msg_server_init_pool.go
+++ x/ledger/keeper/msg_server_init_pool.go
@@ -15,6 +15,11 @@ func (k msgServer) InitPool(goCtx context.Context, msg *types.MsgInitPool) (*typ
 		return nil, sudotypes.ErrCreatorNotAdmin
 	}
 
+	err := k.Keeper.CheckAddress(ctx, msg.Denom, msg.Pool)
+	if err != nil {
+		return nil, err
+	}
+
 	if !k.IsBondedPoolExist(ctx, msg.Denom, msg.Pool) {
 		k.SetExchangeRate(ctx, msg.Denom, sdk.NewInt(0), sdk.NewInt(0))
 		k.AddBondedPool(ctx, msg.Denom, msg.Pool)
