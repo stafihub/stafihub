a80f4ecc71dc9914c3e6fb3bc9f038d2e7045c74
tpkeeper <tpkeeper.me@gmail.com>
2022-08-06 09:21:41 +0800

    bridge: check chainId when add banned denom

--- x/bridge/keeper/msg_server_add_banned_denom.go
+++ x/bridge/keeper/msg_server_add_banned_denom.go
@@ -15,6 +15,10 @@ func (k msgServer) AddBannedDenom(goCtx context.Context, msg *types.MsgAddBanned
 	if !isAdmin {
 		return nil, sudoTypes.ErrCreatorNotAdmin
 	}
+	chainId := uint8(msg.ChainId)
+	if !k.Keeper.HasChainId(ctx, chainId) {
+		return nil, types.ErrChainIdNotSupport
+	}
 
 	k.Keeper.AddBannedDenom(ctx, uint8(msg.ChainId), msg.Denom)
 
